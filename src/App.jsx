e28b8d71fe155ac8b745662370397ae023167122
Akshay <saawant.akshay@gmail.com>
2023-12-21 22:51:02 +0530

    fixed issue given by vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+
 import Rockets from "./components/Rockets";
 import SingleRocket from "./components/SingleRocket";
 import Capsules from "./components/Capsules";
 import Error from "./components/Error";
+import Home from "./components/Home";
 
 function App() {
   return (
