46b2e8b1afc27600d2a9936bf262783c8257a7af
Akshay <saawant.akshay@gmail.com>
2023-12-21 22:43:53 +0530

    fixed some error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+
 import Rockets from "./components/Rockets";
 import SingleRocket from "./components/SingleRocket";
 import Capsules from "./components/Capsules";
+import Home from "./components/Home";
 
 function App() {
   return (
